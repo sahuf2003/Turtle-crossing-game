f4317f832f33bbaffcb9f59ae5de3a329836964f
sahuf2003 <127684377+sahuf2003@users.noreply.github.com>
2024-01-10 20:55:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,56 @@
 from turtle import Screen
 from turt import User
-from car import Car
+from car import Car_manager
 from scoreboard import Scoreboard
 import time
 import random
-color_list = ["blue","green","red","cyan","magenta","yellow","purple"]
-def rax():
-    random_x = random.randint(250, 300)
-    return random_x
+
+
 def ray():
     random_y = random.randint(-200, 250)
     return random_y
+
+
 screen = Screen()
 screen.title("Turtle crossing game")
-screen.setup(600,600)
+screen.setup(600, 600)
 screen.tracer(0)
 scoreboard = Scoreboard()
 user = User()
-cars=[]
-for i in range(10):
-    car = Car()
-    car.color(random.choice(color_list))
-    car.goto(rax(), ray())
-    cars.append(car)
+car_manager = Car_manager()
 screen.listen()
-screen.onkey(user.move,"Up")
+screen.onkey(user.move, "Up")
 game_is_on = True
 while game_is_on:
-    time.sleep(0.3)
-    for i in range(10):
-        cars[i].move()
-        if cars[i].xcor() < -300:
-            cars[i].goto(rax(),ray())
-            cars[i].color(random.choice(color_list))
+    time.sleep(0.1)
+    # for i in range(10):
+    #     cars[i].move()
+    #     if cars[i].xcor() < -300:
+    #         cars[i].goto(rax(),ray())
+    #         cars[i].color(random.choice(color_list))
+    #
+
+    #     user.goto(0,-280)
+    #     for i in range(10):
+    #         cars[i].increase_speed()
+    # for i in cars:
+    #     if user.distance(i) < 28:
+    #         game_is_on = False
+    #         scoreboard.game_over()
+
+    car_manager.create_cars()
+    car_manager.move()
+
+    for car in car_manager.cars:
+        if car.distance(user) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
 
     if user.ycor() > 280:
         scoreboard.increase_score()
-        user.goto(0,-280)
-        for i in range(10):
-            cars[i].increase_speed()
-    for i in cars:
-        if user.distance(i) < 30:
-            game_is_on = False
-            scoreboard.game_over()
+        user.goto(0, -280)
+        car_manager.increase_speed()
     screen.update()
-
 
 screen.exitonclick()
